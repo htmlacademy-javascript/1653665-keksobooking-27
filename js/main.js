b3af70a9268804f43d3ba26095bd049cecbe742d
Deltaqw <etude445@gmail.com>
2022-10-04 17:56:47 +0300

    Фикс поправок

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-function getRandomInRange(min, max,decimalPoint) {
-    if(min < 0 || max < 0) {
-      return NaN;
-    }
-  return (Math.random() * (max - min + 1) + min).toFixed(decimalPoint);
+function getRandomInRange(min, max,decimalPoint = 0) {
+  if(min < 0 || max < 0) {
+    return NaN;
+  }
+  return Number((Math.random() * (max - min + 1) + min).toFixed(decimalPoint));
 
 }
 
-console.log(getRandomInRange(0, 100,4));
+getRandomInRange(0, 100);
