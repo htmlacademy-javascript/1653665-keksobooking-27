adbb768421a2b3106c49c07246bca6017042afc8
Deltaqw <etude445@gmail.com>
2022-11-10 16:06:35 +0300

    zadanie 9.1

--- js/markup-generate.js
+++ js/markup-generate.js
@@ -1,4 +1,9 @@
-import {createAd} from './data.js';
+import{activeForm} from './status-form.js';
+import{createAd} from './data.js';
+
+
+const adressInput = document.querySelector('#address');
+const resetButton = document.querySelector('[type="reset"]');
 const typeApart = {
   flat: 'Квартира',
   bungalow: 'Бунгало',
@@ -9,45 +14,113 @@ const typeApart = {
 };
 
 
-const innerMap = document.querySelector('#map-canvas');
-const generateMarkup = document.querySelector('#card').content.querySelector('.popup');
+const map = L.map('map-canvas')
+  .on('load', () => {
+    activeForm();
+  })
+  .setView({
+    lat: 35.681217,
+    lng : 139.753596
+  }, 12);
 
-const moreAd = createAd[0];
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map);
 
+const mainPinIcon = L.icon({
+  iconUrl:'./img/main-pin.svg',
+  iconSize: [52,52],
+  iconAnchor: [26,52],
 
-const newAd = generateMarkup.cloneNode(true);
-const featureList = newAd.querySelectorAll('.popup__feature');
-const featureItems = moreAd.offer.features;
-
-newAd.querySelector('.popup__title').textContent = moreAd.offer.title;
-newAd.querySelector('.popup__text--address').textContent = moreAd.offer.address;
-newAd.querySelector('.popup__text--price').textContent = `${moreAd.offer.price } ₽/ночь`;
-newAd.querySelector('.popup__type').textContent = typeApart[moreAd.offer.type];
-newAd.querySelector('.popup__text--capacity').textContent = `${moreAd.offer.rooms} комнаты для ${moreAd.offer.guests} гостей`;
-newAd.querySelector('.popup__text--time').textContent = `Заезд после ${moreAd.offer.checkin}, выезд до ${moreAd.offer.checkout}`;
-
-newAd.querySelector('.popup__description').textContent = moreAd.offer.description;
-newAd.querySelector('.popup__avatar').src = moreAd.author.avatar;
-moreAd.offer.photos.forEach((photos) => {
-  const photosContainer = document.createElement('img');
-  photosContainer.classList.add('popup__photo');
-  photosContainer.width = 45;
-  photosContainer.height = 40;
-  photosContainer.alt = 'Фотография жилья';
-  photosContainer.src = photos;
-  newAd.querySelector('.popup__photos').append(photosContainer);
 });
 
-
-featureList.forEach((featureListItem) => {
-  const isNecessary = featureItems.some(
-    (featureItem) => featureListItem.classList.contains(`popup__feature--${ featureItem}`),
-  );
-
-
-  if (!isNecessary) {
-    featureListItem.remove();
-  }
+const subPinIcon = L.icon({
+  iconUrl: './img/main-pin.svg',
+  iconSize: [40,40],
+  iconAnchor:[26,40]
 });
 
-innerMap.appendChild(newAd);
+const balloonTemplate = document.querySelector('#card').content.querySelector('.popup');
+
+const createCustomPopup = (newAd) => {
+  const popupElement = balloonTemplate.cloneNode(true);
+  const featureList = popupElement.querySelectorAll('.popup__feature');
+  const featureItems = newAd.offer.features;
+
+  popupElement.querySelector('.popup__avatar').src = newAd.author.avatar;
+  popupElement.querySelector('.popup__title').textContent = newAd.offer.title;
+  popupElement.querySelector('.popup__text--address').textContent = newAd.offer.address;
+  popupElement.querySelector('.popup__text--price').textContent = `${newAd.offer.price } ₽/ночь`;
+  popupElement.querySelector('.popup__type').textContent = typeApart[newAd.offer.type];
+  popupElement.querySelector('.popup__text--capacity').textContent = `${newAd.offer.rooms} комнаты для ${newAd.offer.guests} гостей`;
+  popupElement.querySelector('.popup__text--time').textContent = `Заезд после ${newAd.offer.checkin}, выезд до ${newAd.offer.checkout}`;
+  popupElement.querySelector('.popup__description').textContent = newAd.offer.description;
+  newAd.offer.photos.forEach((photos) => {
+    const photosContainer = document.createElement('img');
+    photosContainer.classList.add('popup__photo');
+    photosContainer.width = 45;
+    photosContainer.height = 40;
+    photosContainer.alt = 'Фотография жилья';
+    photosContainer.src = photos;
+    popupElement.querySelector('.popup__photos').append(photosContainer);
+  });
+
+  featureList.forEach((featureListItem) => {
+    const isNecessary = featureItems.some(
+      (featureItem) => featureListItem.classList.contains(`popup__feature--${featureItem}`),
+    );
+    if (!isNecessary) {
+      featureListItem.classList.add('hidden');
+    } else {
+      featureListItem.classList.remove('hidden');
+    }
+  });
+  return popupElement;
+};
+
+
+const mainPinMarker = L.marker(
+  {
+    lat: 35.681217,
+    lng: 139.753596
+  },
+  {
+    draggable: true,
+    icon: mainPinIcon,
+  },
+);
+
+mainPinMarker.addTo(map);
+
+mainPinMarker.on('moveend', (evt) => {
+  const coordinate = evt.target.getLatLng();
+  adressInput.value = `${coordinate.lat.toFixed(5)},${coordinate.lng.toFixed(5)}`;
+});
+
+resetButton.addEventListener('click', () => {
+  mainPinMarker.setLatLng({
+    lat: 35.681217,
+    lng: 139.753596
+  });
+
+  map.setView({
+    lat: 35.681217,
+    lng: 139.753596
+  }, 12);
+});
+
+createAd.forEach((ad) =>{
+  const subPinMarker = L.marker({
+    lat: ad.newLocation.lat,
+    lng: ad.newLocation.lng,
+  },
+  {
+    icon: subPinIcon,
+  });
+  subPinMarker
+    .addTo(map)
+    .bindPopup(createCustomPopup(ad));
+});
