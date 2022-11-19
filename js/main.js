8336a5d212f1f3ca740678c5f441bfb654938e8d
Deltaqw <etude445@gmail.com>
2022-11-19 18:51:26 +0300

    11.1

--- js/main.js
+++ js/main.js
@@ -3,5 +3,9 @@ import './markup-generate.js';
 import './status-form.js';
 import './valid-form.js';
 
-
+fetch('https://27.javascript.pages.academy/keksobooking/data')
+  .then((response) => response.json())
+  .then((ad) => {
+    console.log(ad);
+  });
 
